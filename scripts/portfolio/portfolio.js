3ec6bc1f24c972caca82096c8456168aefa8ae75
emaljarilla <emaljarilla@gmail.com>
2024-06-06 02:19:05 +0200

    valoracionesMidu

--- scripts/portfolio/portfolio.js
+++ scripts/portfolio/portfolio.js
@@ -56,7 +56,15 @@ inicioSaber.addEventListener('click',()=>{
     document.documentElement.scrollTop=750;
 })
 
+//disponibilidad
+let disponibilidad = document.querySelector(".estado_laboral");
+let inicial = document.querySelector(".testdiponibilidad");
 
+disponibilidad.addEventListener('mouseover', () => {
+    inicial.innerText ="Disponibilidad inmediata";    
+    disponibilidad.classList.add("estado_laboralDisponible");
+    disponibilidad.classList.remove("estado_laboral");
+})
 // botón carta
 let masInfo = document.getElementById("flecha");
 let mostarMenosInfo = document.getElementById("menos")
